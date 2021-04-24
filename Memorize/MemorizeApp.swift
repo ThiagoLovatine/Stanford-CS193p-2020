b4c85bf53f1fc83929a47a112536f8c2c8f625a9
Thiago Lovatine <thiago.siqueira@firstcontrol.com.br>
2021-04-24 08:31:51 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Thiago Lovatine on 24/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
