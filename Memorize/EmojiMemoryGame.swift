508c0340109004db4f20cd4d0bbb94b55ea0f18b
Thiago Lovatine <thiago.siqueira@firstcontrol.com.br>
2021-04-24 10:14:16 -0300

    ViewModel for EmojiMemoryGame

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Thiago Lovatine on 24/04/21.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String>
+    
+}
